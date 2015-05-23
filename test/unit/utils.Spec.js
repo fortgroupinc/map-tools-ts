f761079105bb5aa38e981071a0d4859dbde44ff5
Yago Ferrer <yago.ferrer@gmail.com>
2015-05-23 08:39:51 -0700

    Adds utils test

--- test/unit/utils.Spec.js
+++ test/unit/utils.Spec.js
@@ -6,8 +6,15 @@ describe('Given the Utils Module', function () {
     config = require('config');
   });
 
-  it('should have a version number', function () {
+  it('should have configuration parameters', function () {
     expect(config.version).to.be.a('string');
+    expect(config.url).to.be.a('string');
+    expect(config.zoom).to.be.a('number');
+    expect(config.customMapOptions).to.be.a('array');
+    expect(config.customMarkerOptions).to.be.a('array');
+    expect(config.panelPosition).to.be.a('string');
+    expect(config.customInfoWindowOptions).to.be.a('array');
+    expect(config.customEvents).to.be.a('array');
   });
 
 });
