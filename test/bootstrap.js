e12589b67d62d8fef651cb8a0ee2bd0156053c23
Yago Ferrer <yago.ferrer@gmail.com>
2015-05-22 21:04:39 -0700

    Adds testing framework

---
+++ test/bootstrap.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Need it for testing
+var chai = require('chai');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+global.expect = chai.expect;
+global.sinon = require('sinon');
+
+// Dependencies
+global.crossfilter = require('crossfilter');
+
+var window = require('../test/mocks/window');
+window.google = require('../test/mocks/google-maps');
+
+window.mapTools = global.mapTools = require('index');
+global.window = window;
