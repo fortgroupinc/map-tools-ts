13bde49de899e19a2a41849bb8b8668af95d244d
Yago Ferrer <yago.ferrer@gmail.com>
2015-05-23 09:51:13 -0700

    Adds utils and tests.

--- test/unit/utils.Spec.js
+++ test/unit/utils.Spec.js
@@ -1,20 +1,84 @@
-describe('Given the Utils Module', function () {
 
-  var config;
-  beforeEach(function() {
+describe('Given the Utils Class', function () {
 
-    config = require('config');
+  var utils;
+
+  beforeEach(function () {
+    utils = require('utils');
   });
 
-  it('should have configuration parameters', function () {
-    expect(config.version).to.be.a('string');
-    expect(config.url).to.be.a('string');
-    expect(config.zoom).to.be.a('number');
-    expect(config.customMapOptions).to.be.a('array');
-    expect(config.customMarkerOptions).to.be.a('array');
-    expect(config.panelPosition).to.be.a('string');
-    expect(config.customInfoWindowOptions).to.be.a('array');
-    expect(config.customEvents).to.be.a('array');
+
+  describe('when calling clone()', function () {
+    it('should clone an object', function() {
+      var obj = {
+        property1: 'value1'
+      };
+
+      var output = utils.clone(obj);
+      expect(output).to.eql(obj);
+      obj.property1 = 'value2';
+      expect(output.property1).to.eql('value1');
+    });
+
+    describe('with a an array of keys as second parameter', function () {
+
+      it('should not clone the exception keys', function() {
+        var obj = {
+          property1: 'value1',
+          property2: 'value2',
+          property3: 'value3'
+        };
+
+
+        var output = utils.clone(obj, ['property2', 'property3']);
+        expect(output).to.eql({property1: 'value1'});
+      });
+
+    });
+
   });
 
+
+  describe('when calling isArray', function () {
+
+    it('should return True when the input is an Array', function() {
+
+      var result1 = utils.isArray([]);
+      expect(result1).to.eql(true);
+
+    });
+
+
+    it('should return False when the input is not an Array', function() {
+      var result = utils.isArray('string');
+      expect(result).to.eql(false);
+    });
+
+  });
+
+
+  describe('when calling defaultDimension()', function () {
+    describe('if the property exists in the root level', function () {
+      it('should return the value of the property', function () {
+        var result = utils.defaultDimension('visible')({data:{}, visible: true});
+        expect(result).to.eql(true);
+      });
+
+    });
+    describe('if the property exists under "data"', function () {
+      it('should return the value of the data.property', function () {
+        var result = utils.defaultDimension('visible')({data:{visible: true}});
+        expect(result).to.eql(true);
+      });
+    });
+
+    describe('if the property does not exist anywhere', function () {
+
+      it('should return null', function () {
+        var result = utils.defaultDimension('visible')({data:{}});
+        expect(result).to.eql(null);
+      });
+
+    });
+  });
 });
