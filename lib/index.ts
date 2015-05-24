1eeee13dcc1f6a10d4ea090a4bd45e802e58f84a
Yago Ferrer <yago.ferrer@gmail.com>
2015-05-23 17:17:12 -0700

    Adds tests for locate

--- lib/index.ts
+++ lib/index.ts
@@ -3,12 +3,15 @@ class Index {
 
   public instance = {};
   public addMarker;
+  public locate;
   public crossfilter = require('crossfilter');
 
   constructor(options, cb) {
 
     this.instance = {good: 'instance'};
     this.addMarker = new AddMarker().add;
+
+    this.locate = new Locate().locate;
 
     var map = new AddMap(this);
 
--- lib/references.ts
+++ lib/references.ts
@@ -3,5 +3,6 @@
 /// <reference path="utils.ts"/>
 /// <reference path="addMap.ts"/>
 /// <reference path="addMarker.ts"/>
+/// <reference path="locate.ts"/>
 /// <reference path="index.ts"/>
 
